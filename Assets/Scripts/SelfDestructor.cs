b71d21de5801d4dc73e480f393bfb91ea11cba61
Teyhouse <teyhouse@googlemail.com>
2015-06-17 19:18:11 +0200

    Fixed Explosion Bug
    Explosions-Bug gefixt -Explosions-Prefab wird nun nach 3 Sekunden
    zerstört

---
+++ Assets/Scripts/SelfDestructor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfDestructor : MonoBehaviour {
+
+	//Selbstzerstörung
+	public int LiveTime;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, LiveTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
